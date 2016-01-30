364b7c1aea1bfc8592f78d32f64f42a573528e04
Arthur Veys <veysarthur@gmail.com>
2016-01-30 02:12:41 +0100

    menu

---
+++ GGJ2016/Assets/Script/menuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void ExitPress(){
+		Application.Quit();
+	}
+
+	public void startGame(){
+		Application.LoadLevel (1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
